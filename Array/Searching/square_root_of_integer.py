1a0ef3001a8e1d6cabad82f5ec5650b3b18c9d95
siddhi-handore <siddhihandore3011@gmail.com>
2025-10-09 22:45:49 +0530

    Square root

---
+++ Array/Searching/square_root_of_integer.py
@@ -0,0 +1,16 @@
+def square(n):
+    low = 1
+    high = n
+    ans = 1
+    while low <= high:
+        mid = (low+high)//2
+        if (mid * mid) <= n:
+            ans = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+    return ans
+
+if __name__ == '__main__':
+    n = 24
+    print(square(8))
